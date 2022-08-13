7836492c93113ac4b0b3b9fe8eca0e3243f008a9
Li Kui <90845831+likui628@users.noreply.github.com>
2022-08-13 20:17:28 +0800

    feat: player sprite and controls

--- main.py
+++ main.py
@@ -17,10 +17,39 @@ BLUE = (0, 0, 255)
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Shmup!")
 clock = pygame.time.Clock()
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -5
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 5
+
+        self.rect.x += self.speedx
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+
+
 all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
 # Game loop
 running = True
 while running:
@@ -31,7 +60,6 @@ while running:
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
-
     # Update
     all_sprites.update()
 
